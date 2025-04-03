7bba078e2c1c8e03c019d37ca386e5bf5cc2f366
Yerassyl Telman <126333891+ErasylTelman@users.noreply.github.com>
2025-04-03 17:20:20 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,21 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_histogram(filename, bins=100):
-    data = np.loadtxt(filename, usecols=0)
+#This code was used to create figures in the prootocol
+
+#function for plotting histograms of semi-major axis distribution
+def plot_histogram_grid(filenames, bins=500):
+    n_rows = 4 
+    n_cols = 3 
     
-    data = data[(data >= 2) & (data <= 3.5)]
-    plt.figure(figsize=(8, 6))
-    plt.hist(data, bins=bins, edgecolor='black', alpha=0.7)
-    plt.xlabel('Semi-Major Axis (a)', fontsize=14)
-    plt.ylabel('Number of Bodies', fontsize=14)
-    plt.title('Histogram of Semi-Major Axis Values', fontsize=16)
-    plt.grid(True, linestyle='--', alpha=0.5)
-    plt.axvline(x = 2.502, linestyle = '--', color = 'red')
-    plt.axvline(x = 2.825, linestyle = '--', color = 'red')
-    plt.axvline(x = 2.958, linestyle = '--', color = 'red')
-    plt.axvline(x = 3.279, linestyle = '--', color = 'red')
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 12), constrained_layout=True)
+    axes = axes.flatten()
+
+    for i, filename in enumerate(filenames):
+        data = np.loadtxt(filename, usecols=0)
+        data = data[(data >= 2) & (data <= 3.6)]
+        
+        axes[i].hist(data, bins=bins)
+        axes[i].axvline(x=2.502, linestyle='--', color='red')
+        axes[i].axvline(x=2.825, linestyle='--', color='red')
+        axes[i].axvline(x=2.958, linestyle='--', color='red')
+        axes[i].axvline(x=3.279, linestyle='--', color='red')
+
+        axes[i].set_title(f't = {i}e5 Myr', fontsize=14)
+        if i >= 8:
+            axes[i].set_xlabel('Semi-Major Axis (AU)', fontsize=12)
+        if i % n_cols == 0:
+            axes[i].set_ylabel('Number of Bodies', fontsize=12)
+        axes[i].grid(True, linestyle='--', alpha=0.5)
     
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.suptitle('Histograms of Semi-Major Axis Values Over Time', fontsize=20)
     plt.show()
 
-plot_histogram('orbits_t10.txt', bins=100)
+filenames = [f'orbits_t{i}.txt' for i in range(11)]
+plot_histogram_grid(filenames, bins=500)
+
+#function for plotting scatter plots of eccentricity vs semi-major axis
+def plot_scatter_grid(filenames):
+    n_rows = 4
+    n_cols = 3 
+    
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 12), constrained_layout=True)
+    axes = axes.flatten()
+
+    for i, filename in enumerate(filenames):
+        a_data = np.loadtxt(filename, usecols=0)
+        e_data = np.loadtxt(filename, usecols=1)
+
+        mask = (a_data >= 2) & (a_data <= 3.6)
+        a_data_filtered = a_data[mask]
+        e_data_filtered = e_data[mask]
+
+        axes[i].scatter(a_data_filtered, e_data_filtered, s=5)
+        axes[i].axvline(x=2.502, linestyle='--', color='red')
+        axes[i].axvline(x=2.825, linestyle='--', color='red')
+        axes[i].axvline(x=2.958, linestyle='--', color='red')
+        axes[i].axvline(x=3.279, linestyle='--', color='red')
+
+        axes[i].set_title(f't = {i}e5 Myr', fontsize=14)
+        if i >= 8:
+            axes[i].set_xlabel('Semi-Major Axis (AU)', fontsize=12)
+        if i % n_cols == 0:
+            axes[i].set_ylabel('Eccentricity', fontsize=12)
+        axes[i].grid(True)
+
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.suptitle('Eccentricity vs. Semi-Major Axis Over Time', fontsize=20)
+    plt.show()
+
+filenames = [f'orbits_t{i}.txt' for i in range(11)]
+plot_scatter_grid(filenames)
